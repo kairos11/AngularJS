ee260993a889d69fb01e105b6e78437479297a49
Tyn Guardian <orange_pumpkin@live.com>
2019-01-05 22:06:55 +0800

    Lesson 6: AngularJS Form Validations

---
+++ AngularJS_FirstProgram/AngularJS_FormValidations/scripts/script.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.js" />
+/* Drag & drop angular.js on the scripts forlder*/
+
+var app = angular.module("myModule", []);
+
+app.controller("formController", function ($scope) {
+    $scope.reset = function () {
+        $scope.firstName = "";
+        $scope.lastName = "";
+        $scope.email = "";
+    }
+    $scope.submit = function () {
+        alert("Ok");
+    }
+
+});
