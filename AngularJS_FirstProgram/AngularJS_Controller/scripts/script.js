a88039a2e720a55dcb66a51582351bb08205cda1
Tyn Guardian <orange_pumpkin@live.com>
2019-01-05 18:52:54 +0800

    Finish Lesson 3 AngularJS Expression Lesson 4 AngularJS Controller Lesson 5 AngularJS Filter

---
+++ AngularJS_FirstProgram/AngularJS_Controller/scripts/script.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../angularjs_firstprogram/scripts/angular.js" /> 
+/* Drag & drop angular.js on the scripts forlder*/
+
+var app = angular.module("myModule", []);
+
+app.controller("firstController", function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+});
+
+app.controller("dataController", function ($scope) {
+    var listEmployees = [
+        { firstName:"Juan",    lastName:"dela Cruz", gender:"Male", salary: 3700 },
+        { firstName:"Juana",   lastName:"dela Cruz", gender:"Female", salary: 36000 },
+        { firstName:"Julio",   lastName:"dela Cruz", gender:"Male", salary: 35000 },
+        { firstName:"Julia",   lastName:"dela Cruz", gender:"Female", salary: 50000 },
+        { firstName:"Juaning", lastName:"dela Cruz", gender:"Female", salary: 80000 }
+    ];
+    $scope.employees = listEmployees;
+});
