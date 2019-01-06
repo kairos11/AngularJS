494e501a28a429b7cfbee3e85db207295ad2f3ca
Tyn Guardian <orange_pumpkin@live.com>
2019-01-06 16:09:51 +0800

    Lesson 7: AngularJS Includes Lesson 8: AnuglarJS AJAX

---
+++ AngularJS_FirstProgram/AngularJS_AJAX/scripts/script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../angularjs_firstprogram/scripts/angular.js" /> 
+/* Drag & drop angular.js on the scripts forlder*/
+
+var app = angular.module("myModule", []);
+
+app.controller("recordsController", function ($scope,$http) { /*$http is used to be able to perform "get" operation on the .json files*/
+    var url = "data/records.json";
+    $http.get(url).success(function (response) {
+        $scope.employees = response;
+    });
+});
